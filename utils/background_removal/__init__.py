aa21dedd161ab44b6b97587a88a1df2afa386b59
Maki <sunwood.ai.labs@gmail.com>
2025-03-22 04:13:55 +0900

    #5 関数名を変更：find_and_remove_largest_rectangle  find_and_remove_large_paths

--- utils/background_removal/__init__.py
+++ utils/background_removal/__init__.py
@@ -4,12 +4,12 @@ Background removal module for Animal Grid Vectorizer.
 
 from .simple import remove_background_simple
 from .advanced import remove_background_advanced
-from .svg_cleanup import find_and_remove_largest_rectangle
+from .svg_cleanup import find_and_remove_large_paths
 from .batch import batch_remove_background
 
 __all__ = [
     'remove_background_simple',
     'remove_background_advanced',
-    'find_and_remove_largest_rectangle',
+    'find_and_remove_large_paths',
     'batch_remove_background'
 ]
